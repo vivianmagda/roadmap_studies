c5fe6148973821a5c49a1d7146ed351e4a35a8c5
vivianmagda <vivianmagda@live.com>
2025-03-24 22:52:52 -0300

    refactoring

--- src/main/java/blocks/main.java
+++ src/main/java/blocks/main.java
@@ -1,6 +1,6 @@
 package blocks;
 
-public class main{
+public class Main{
 
     public static void main(String[] args) {
         System.out.println("Inside the main method");
---
+++ src/main/java/lambda_expressions/Main.java
@@ -0,0 +1,7 @@
+package lambda_expressions;
+
+public class Main {
+
+
+
+}
